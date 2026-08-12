ee0962ce262435f795573bf524414d759adebe0d
amulya152005 <bodduamulya08@gmail.com>
2026-08-12 11:18:52 +0530

    hello

---
+++ multiplication.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 6, num2 = 7;
+    int product = num1 * num2;
+
+    printf("The product is: %d\n", product);
+
+    return 0;
+}
---
+++ subtract.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 8;
+
+        int result = num1 - num2;
+
+        System.out.println("The subtraction is: " + result);
+    }
+}
